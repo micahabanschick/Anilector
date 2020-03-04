81b5611d7f82d26d8ce76cedb7b15aa8a4da02c4
Micah Banschick <micha.ban@gmail.com>
2020-03-04 13:52:06 -0500

    scraper_spec Modified

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -13,4 +13,34 @@ describe "Scraper" do
       end
     end
 
+    describe "#top_anime" do
+        it "will scrape the top 20 anime off of MAL" do
+          new_method = Scraper.new
+    
+          new_method_method = new_method.instance_variable_get(:@name)
+    
+          expect(new_song_name).to eq("Something")
+        end
+      end
+
+      describe "#top_anime_from_each_genre" do
+        it "will scrape the top 3 anime from each genre on MAL into a hash" do
+          new_method = Scraper.new
+    
+          new_method_method = new_method.instance_variable_get(:@name)
+    
+          expect(new_song_name).to eq("Something")
+        end
+      end
+
+      describe "#top_entertainment" do
+        it "will scrape the user's 5 given movies or shows from IMBd" do
+          new_method = Scraper.new
+    
+          new_method_method = new_method.instance_variable_get(:@name)
+    
+          expect(new_song_name).to eq("Something")
+        end
+      end
+
 end 

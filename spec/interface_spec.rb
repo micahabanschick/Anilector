57c00d61238bf713bf5e1d166425053594569840
Micah Banschick <micha.ban@gmail.com>
2020-03-04 12:22:51 -0500

    Interface lib&spec files

---
+++ spec/interface_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../config/environment.rb'
+
+describe "Interface" do
+    let(:song) { Song.new("In the Aeroplane Over the Sea") }
+  
+    describe "#to_be_determined" do
+      it "will be decided in the future" do
+        new_method = Interface.new
+  
+        new_method_method = new_method.instance_variable_get(:@name)
+  
+        expect(new_song_name).to eq("Something")
+      end
+    end
+
+end 

ea0d6b2dbd26244251ba74f622fdd062b92c7312
Micah Banschick <micha.ban@gmail.com>
2020-03-05 09:10:12 -0500

    user_spec Modified

--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,20 @@
 class User
+
+    attr_accessor :name, :genres
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        self.save 
+    end 
+
+    def self.save 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all
+    end 
+
 end 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,11 +2,11 @@ require_relative '../config/environment.rb'
 require_relative './spec_helper.rb'
 
 describe "User" do
-    let(:song) { Song.new("In the Aeroplane Over the Sea") }
+    #let(:song) { Song.new("In the Aeroplane Over the Sea") }
   
     describe "#to_be_determined" do
         it "will be decided in the future" do
-        new_method = Scraper.new
+        new_method = User.new
 
         new_method_method = new_method.instance_variable_get(:@name)
 
@@ -14,9 +14,17 @@ describe "User" do
         end
     end
 
+    describe "@@all" do
+        it "is initialized as an empty array" do
+          all = User.class_variable_get(:@@all)
+    
+          expect(all).to match_array([])
+        end
+    end
+
     describe "#initialize" do
         it "will create new user" do
-            new_method = User.new
+            new_method = User.new("Bob")
 
             new_method_method = new_method.instance_variable_get(:@name)
 
@@ -24,15 +32,15 @@ describe "User" do
         end
 
         it "will initialize with a name" do
-            new_method = User.new
+            new_method = User.new("Katy")
 
-            new_method_method = new_method.instance_variable_get(:@name)
+            new_method_name = new_method.instance_variable_get(:@name)
 
-            expect(new_song_name).to eq("Something")
+            expect(new_method_name).to eq("Katy")
         end
         
         it "will save user to @@all" do
-            new_method = User.new
+            new_method = User.new("Bob")
         
             new_method_method = new_method.instance_variable_get(:@name)
         
@@ -40,33 +48,33 @@ describe "User" do
         end
     end
 
-    describe "#self.save" do
+    describe ".save" do
         it "will save self to @@all" do
-        new_method = User.new
+        new_method = User.new("Bob")
 
         new_method_method = new_method.instance_variable_get(:@name)
 
-        expect(new_song_name).to eq("Something")
+        expect(new_method_method).to eq("Something")
         end
     end
 
-    describe "#self.all" do
-        it "will read @@all" do
-        new_method = User.new
-
-        new_method_method = new_method.instance_variable_get(:@@all)
-
-        expect(new_song_name).to eq("Something")
+    describe ".all" do
+        it "returns the class variable @@all" do
+          expect(User.all).to match_array([])
+    
+          User.class_variable_set(:@@all, [song])
+    
+          expect(User.all).to match_array([song])
         end
     end
 
     describe "#genres" do
         it "will read @genres as array" do
-        new_method = User.new
+        new_method = User.new("Bob")
 
-        new_method_method = new_method.instance_variable_get(:@name)
+        new_method_method = new_method.instance_variable_get(:@genres)
 
-        expect(new_song_name).to eq("Something")
+        expect(new_method_method).to eq("Something")
         end
     end
 

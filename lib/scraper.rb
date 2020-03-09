e525a5294a9ac07b7ff44f510db7845516b9cec4
Micah Banschick <micha.ban@gmail.com>
2020-03-08 21:17:20 -0400

    Scraper: #inner_hash;#inner_array;#outer_hash

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,18 +2,19 @@ class Scraper
 
 
     def anime_from_each_genre(site, genres)
+        #an array whose elements reveal the information for 5 anime from each given genre
         #binding.pry 
-        document_1 = doc(genre_site(site,genres,0))
-        name = anime_name(site, genres, 0, 0) 
-        genress = anime_genres(site, genres, 0, 0)
+        [outer_hash(site, genres, 0), outer_hash(site, genres, 1), outer_hash(site, genres, 2)]
         #binding.pry
     end 
 
     def appendage(site, genres, index)
+        #end piece added on to the site to get to the genre's page
         doc(site).css("a.genre-name-link").find{|genre| genre.text.match(genres[index])}.attributes.values[0].value
     end 
 
     def genre_site(site, genres, index)
+        #url for genre's page
         site.gsub("/anime.php", appendage(site, genres, index))
     end 
 
@@ -22,13 +23,33 @@ class Scraper
     end
 
     def anime_name(site, genres, doc_index, anime_index)
+        #shortcut to get to the anime's name
         document = doc(genre_site(site, genres, doc_index))
         document.css("p.title-text")[anime_index].text.strip
     end 
 
     def anime_genres(site, genres, doc_index, anime_index)
+        #shortcut to get an array of the anime's genres
         document = doc(genre_site(site, genres, doc_index))
         document.css("div.genres-inner.js-genre-inner")[anime_index].children.map{|genre| genre.text.strip}
+    end
+
+    def inner_hash(site, genres, doc_index, anime_index)
+        {:name=>anime_name(site, genres, doc_index, anime_index), :genres=>anime_genres(site, genres, doc_index, anime_index)}
+    end
+
+    def inner_array(site, genres, doc_index)
+        i = 0
+        arr = []
+        while i < 5 do 
+            arr << inner_hash(site, genres, doc_index, i)
+            i += 1
+        end
+        arr 
+    end 
+
+    def outer_hash(site, genres, index)
+        {:genre=>genres[index], :anime=>inner_array(site, genres, index)}
     end
 #binding.pry
 end 
